0ddb4985e4f512479e9ac4d9633e0d2b54016b16
mohammadrabetian <tommyrabetian@gmail.com>
2020-05-16 00:39:52 +0430

    get all messages rpc

--- temp_messenger/dependencies/redis.py
+++ temp_messenger/dependencies/redis.py
@@ -8,35 +8,38 @@ class RedisError(Exception):
     pass
 
 
-
 class RedisClient:
     def __init__(self, url):
-        self.redis = StrictRedis.from_url(
-            url, decode_responses=True
-        )
+        self.redis = StrictRedis.from_url(url, decode_responses=True)
 
     def get_message(self, message_id: int) -> str:
         message = self.redis.get(message_id)
 
         if message is None:
-            raise RedisError(f'Message Not Found: {message_id}')
-        
+            raise RedisError(f"Message Not Found: {message_id}")
+
         return message
-    
+
     def save_message(self, message):
         message_id = uuid4().hex
         self.redis.set(message_id, message)
 
         return message_id
 
+    def get_all_messages(self):
+        return [
+            {"id": message_id, "message": self.redis.get(message_id)}
+            for message_id in self.redis.keys()
+        ]
+
 
 class MessageStore(DependencyProvider):
     def setup(self):
-        redis_url = self.container.config['REDIS_URL']
+        redis_url = self.container.config["REDIS_URL"]
         self.client = RedisClient(redis_url)
-    
+
     def stop(self):
         del self.client
-    
+
     def get_dependency(self, worker_ctx):
         return self.client
