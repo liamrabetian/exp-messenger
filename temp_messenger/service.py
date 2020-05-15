1e246056463a49c6d6e24fec33c4e2029c0e7e85
mohammadrabetian <tommyrabetian@gmail.com>
2020-05-15 21:27:54 +0430

    http microservice, home page

--- temp_messenger/service.py
+++ temp_messenger/service.py
@@ -1,4 +1,5 @@
-from nameko.rpc import rpc
+from nameko.rpc import rpc, RpcProxy
+from nameko.web.handlers import http
 
 class HelloService:
 
@@ -7,3 +8,13 @@ class HelloService:
     @rpc
     def hello(self):
         return 'hello!'
+
+
+class WebServer:
+
+    name = 'web_server'
+    hello_service = RpcProxy('hello_service')
+
+    @http('GET', '/')
+    def home(self, request):
+        return self.hello_service.hello()
